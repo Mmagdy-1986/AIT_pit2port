d62042b9bb521196c8a49709c67a6a8cc8bf30f8
Mmagdy-1986 <mmagdy@info-ait.com>
2026-07-05 19:18:59 +0300

    Update CACHE_NAME for service worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ait-rental-mobile-v2';
+const CACHE_NAME = 'ait-rental-professional-v3';
 const APP_FILES = ['./','./index.html','./css/style.css','./js/app.js','./manifest.json','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
