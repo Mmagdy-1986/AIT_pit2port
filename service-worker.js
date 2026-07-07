9237ba293ec42590779e3ef5cd8270b1e792ace5
Mmagdy-1986 <mmagdy@info-ait.com>
2026-07-08 01:35:18 +0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME = 'ait-rental-professional-v3';
-const APP_FILES = ['./','./index.html','./css/style.css','./js/app.js','./manifest.json','./assets/icon-192.png','./assets/icon-512.png'];
-self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))); self.skipWaiting(); });
-self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
-self.addEventListener('fetch', event => { event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))); });
